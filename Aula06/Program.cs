4774c494530e5cd7b2799c4c1e5913eb13317660
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-02-12 19:12:10 -0300

    Aula 06

---
+++ Aula06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula06
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------- Verificador de números pares ou ímpares -----------");
+
+            Console.WriteLine("Digite um número: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("O número é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar.");
+            }
+        }
+    }
+}
