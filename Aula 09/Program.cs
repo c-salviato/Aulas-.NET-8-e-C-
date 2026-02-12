916bd2df21f5a5964abe9c085edf9d3f5f3490a7
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-02-12 19:44:45 -0300

    Aula 09

---
+++ Aula 09/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-------- Operadores Lógicos --------");
+
+        bool isLogged = true;
+        bool hasAdminAccess = false;
+
+        Console.WriteLine("\nInformações Usuário:");
+        Console.WriteLine("Usuário Logado " + isLogged);
+        Console.WriteLine("Acesso ao painel de adminstrador: " + hasAdminAccess);
+
+        // Operador (||) OR
+        if (isLogged || hasAdminAccess)
+        {
+            Console.WriteLine("Está logado");
+        }
+        else
+        {
+            Console.WriteLine("Não está logado");
+        }
+
+        // Operador (&&) AND
+        if (isLogged && hasAdminAccess)
+        {
+            Console.WriteLine("Acesso ao painel de administrador consedido");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao painel de administrador negado");
+        }
+
+        // Operador de negação (!) NO
+        if (!isLogged)
+        {
+            Console.WriteLine("Não está logado");
+        }
+        else
+        {
+            Console.WriteLine("Está logado");
+        }
+    }
+}
