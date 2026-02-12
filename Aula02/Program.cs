3c29fead0f2af621f2ce08034cc4be6ddec2ef78
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-02-12 18:47:25 -0300

    Aulas 1,2,3 adicionadas

---
+++ Aula02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Caique";
+        Console.WriteLine(myName);
+
+        int number = 20;
+        Console.WriteLine(number);
+
+        double number_db = 10.5;
+        Console.WriteLine(number_db);
+
+        char character = 'a';
+        Console.WriteLine(character);
+
+    }
+}
---
+++ Aula03/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+        //VARIAVEL (TIPO INT)
+
+        int number = 30;
+
+        number = number / 10;
+
+        Console.WriteLine("O valor antigo da variável era: " + number);
+    
+        number = number * 4;
+
+        Console.WriteLine("Agora o valor novo é: " + number);
+
+        // CONSTANTE
+
+        const int number2 = 1000; //não é possível alterar esse valor
+
+        Console.WriteLine(number2);
+
+        // se colocar "var" no tipo da variável, o próprio código reconhece automaticamente qual é o tipo ideal (só a primeira atribuição vale, depois não da para trocar o tipo da variável)
+
+        var nome = "Lucas Neto";
+
+        Console.WriteLine(nome);
+
+        // variável dinâmica, basicamente pode trocar de tipo da variavel (ex: de INT para STRING)
+
+        dynamic test = 20;
+
+        test = 30;
+        test = "Marcelão";
+
+        Console.WriteLine(test);
+
+
+
+        
+        
+    }
+}
