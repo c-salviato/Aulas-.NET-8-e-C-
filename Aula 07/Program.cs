bffcb54cfe41f2f00a4eac21cb29fc777c6eaa26
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-02-12 19:20:11 -0300

    Aula 07

---
+++ Aula 07/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Aula07
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------- Operadores de Atribuição -----------");
+
+            // inicializar a variável
+            int x = 10;
+            Console.WriteLine("Valor inicial é : " + x);
+
+            // Operador de atribuição simples
+            x = 12;
+            Console.WriteLine("Atribuição Simples (=): " + x);
+
+            // Atribuição Composta
+            x = x + 10;
+            Console.WriteLine("Atribuição Composta (x = x + 10): " + x);
+
+            // Atribuição composta com ++
+            x++;
+            Console.WriteLine("Atribuição Composta (x++): " + x);
+
+            // Atribuição composta com +=
+            x += 15;
+            Console.WriteLine("Atribuição Composta (x += 15): " + x);
+
+            // Atribuição composta com *=
+            x *= 15;
+            Console.WriteLine("Atribuição Composta (x *= 15): " + x);
+
+            // Atribuição composta com /=
+            x /= 15;
+            Console.WriteLine("Atribuição Composta (x /= 15): " + x);
+        }
+    }
+}
