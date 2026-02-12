4ee57bc38655cb2dd3b17a49557be136a0e7654d
Caique Souza Salviato <caique.s.salviato@gmail.com>
2026-02-12 18:56:17 -0300

    Aula 04

---
+++ Aula04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("---------- Cadastro de Usuário ----------");
+
+        //Solicitar nome do usuário
+
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+
+        //Solicitar a Idade do Usuário
+        Console.WriteLine("Digite sua Idade: ");
+        int idd = Convert.ToInt32(Console.ReadLine());
+
+        // Imprimindo dados no console
+        Console.WriteLine("---------- Dados do usuário ----------");
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("idade: " + idd);
+        
+    }
+}
